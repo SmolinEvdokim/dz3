cb6787ec48085f3a6bcb1381c5c9f4699460fec0
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-23 20:30:33 +0100

    3

---
+++ 3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+// 3 -> [1, 8, 27].
+// 5 -> [1, 8, 27, 64, 125]
+// Указание: Вывод массива вынести в отдельную функцию.
+
+Console.Write("Vvedite 4isli N: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int count = coll.Length;
+  int index = 0;
+  while(index < count)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] array = new int[cube+1];
+Cube(array);
+PrintArry(array);
